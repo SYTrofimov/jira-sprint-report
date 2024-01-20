598fdb9d2cba5aae38ac3692b45aba34df7ece4c
Sergey Trofimov <trofimov.s@gmail.com>
2024-01-20 14:25:56 +0100

    Start velocity test

---
+++ src/test-velocity.js
@@ -0,0 +1,51 @@
+'use strict';
+import fs from 'fs';
+
+async function testVelocity() {
+  const boards = JSON.parse(fs.readFileSync('data/boards.json', 'utf8'));
+  for (const board of boards) {
+    await testVelocityOnBoard(board);
+  }
+}
+
+async function testVelocityOnBoard(board) {
+  console.log(`Testing velocity on board ${board.id} - ${board.name}`);
+
+  const boardPath = `data/board-${board.id}`;
+
+  const sprints = await jiraGetItems(`rest/agile/1.0/board/${board.id}/sprint`);
+  fs.writeFileSync(boardPath + '/sprints.json', JSON.stringify(sprints, null, 2));
+  console.log(`Saved ${sprints.length} board sprints`);
+
+  // Get GreenHopper velocity report
+  const velocity = await jiraGet(
+    `rest/greenhopper/1.0/rapid/charts/velocity?rapidViewId=${board.id}`,
+  );
+  fs.writeFileSync(boardPath + '/velocity.json', JSON.stringify(velocity, null, 2));
+
+  for (const sprint of sprints) {
+    await testVelocityOnSprint(board, sprint);
+  }
+}
+
+async function testVelocityOnSprint(board, sprint) {
+  console.log(`Saving sprint ${sprint.id} - ${sprint.name}`);
+
+  const sprintPathPrefix = `data/board-${board.id}/${sprint.id}-`;
+
+  // Get sprint issues
+  const issues = await jiraGetItems(
+    `rest/agile/1.0/board/${board.id}/sprint/${sprint.id}/issue?expand=changelog`,
+    'issues',
+  );
+  fs.writeFileSync(sprintPathPrefix + 'sprint-issues.json', JSON.stringify(issues, null, 2));
+  console.log(`Saved ${issues.length} issues`);
+
+  // Get GreenHopper sprint report
+  const report = await jiraGet(
+    `rest/greenhopper/1.0/rapid/charts/sprintreport?rapidViewId=${board.id}&sprintId=${sprint.id}`,
+  );
+  fs.writeFileSync(sprintPathPrefix + 'sprintreport.json', JSON.stringify(report, null, 2));
+}
+
+await testVelocity();

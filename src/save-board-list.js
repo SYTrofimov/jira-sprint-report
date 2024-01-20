105d429f5dfb6f3fca0c55abcd152f24df6ff249
Sergey Trofimov <trofimov.s@gmail.com>
2024-01-20 15:37:01 +0100

    Create 'data/' if doesn't exist

--- src/save-board-list.js
+++ src/save-board-list.js
@@ -4,6 +4,10 @@ import fs from 'fs';
 import { jiraGetItems } from './utils.js';
 
 async function saveBoardList() {
+  if (!fs.existsSync('data')) {
+    fs.mkdirSync('data');
+  }
+
   const boards = await jiraGetItems(`rest/agile/1.0/board`);
   fs.writeFileSync('data/boards.json', JSON.stringify(boards, null, 2));
   console.log('Saved board list');

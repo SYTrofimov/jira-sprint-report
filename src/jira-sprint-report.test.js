c6955d1b424ea380f65b09f750a1597045a48c70
Sergey Trofimov <trofimov.s@gmail.com>
2024-02-11 16:39:49 +0100

    Renamed constants for clarity

--- src/jira-sprint-report.test.js
+++ src/jira-sprint-report.test.js
@@ -37,11 +37,11 @@ const SPRINT1 = {
   createdDate: '2024-01-10T09:00:17.954Z',
 };
 
-const BEFORE_SPRINT = '2023-12-28T15:36:42.765+0000';
+const BEFORE_SPRINT1 = '2023-12-28T15:36:42.765+0000';
 const DURING_SPRINT1 = '2024-01-15T15:36:42.765+0000';
-const DURING_SPRINT2 = '2024-01-16T15:36:42.765+0000';
-const JUST_AFTER_SPRINT_COMPLETE = '2024-01-24T09:30:49.765+0000';
-const AFTER_SPRINT = '2024-01-27T15:36:42.765+0000';
+const DURING_SPRINT1_2 = '2024-01-16T15:36:42.765+0000';
+const JUST_AFTER_SPRINT1_COMPLETE = '2024-01-24T09:30:49.765+0000';
+const AFTER_SPRINT1 = '2024-01-27T15:36:42.765+0000';
 
 const SPRINT2 = {
   id: 6,
@@ -203,7 +203,7 @@ describe('jiraSprintReport', () => {
 
   test('Story Points changed before sprint', () => {
     const issue = makeMinimalIssue();
-    addStoryPointChange(issue, 3, 5, BEFORE_SPRINT);
+    addStoryPointChange(issue, 3, 5, BEFORE_SPRINT1);
 
     const result = issueSprintReport(issue, SPRINT1);
     expect(result.initialEstimate).toBe(5);
@@ -240,7 +240,7 @@ describe('jiraSprintReport', () => {
 
   test('Story Points changed after sprint', () => {
     const issue = makeMinimalIssue();
-    addStoryPointChange(issue, 3, 5, AFTER_SPRINT);
+    addStoryPointChange(issue, 3, 5, AFTER_SPRINT1);
 
     const result = issueSprintReport(issue, SPRINT1);
     expect(result.initialEstimate).toBe(3);
@@ -271,7 +271,7 @@ describe('jiraSprintReport', () => {
     issue.fields.status = {
       name: 'To Do',
     };
-    addSprintChange(issue, '', SPRINT1.id, BEFORE_SPRINT);
+    addSprintChange(issue, '', SPRINT1.id, BEFORE_SPRINT1);
 
     const result = issueSprintReport(issue, SPRINT1);
     expect(result.outcome).toBe('NOT_COMPLETED');
@@ -283,7 +283,7 @@ describe('jiraSprintReport', () => {
     issue.fields.status = {
       name: 'Done',
     };
-    addSprintChange(issue, '', SPRINT1.id, BEFORE_SPRINT);
+    addSprintChange(issue, '', SPRINT1.id, BEFORE_SPRINT1);
     addStatusChange(issue, 'To Do', 'Done', DURING_SPRINT1);
 
     const result = issueSprintReport(issue, SPRINT1);
@@ -298,7 +298,7 @@ describe('jiraSprintReport', () => {
       name: 'Done',
     };
     addSprintChange(issue, '', SPRINT1.id, DURING_SPRINT1);
-    addStatusChange(issue, 'To Do', 'Done', DURING_SPRINT2);
+    addStatusChange(issue, 'To Do', 'Done', DURING_SPRINT1_2);
 
     const result = issueSprintReport(issue, SPRINT1);
     expect(result.outcome).toBe('COMPLETED');
@@ -310,7 +310,7 @@ describe('jiraSprintReport', () => {
     issue.fields.customfield_sprint.push(SPRINT1);
     issue.fields.customfield_sprint.push(SPRINT2);
     addSprintChange(issue, SPRINT2.id, SPRINT1.id, DURING_SPRINT1);
-    addSprintChange(issue, SPRINT1.id, `${SPRINT1.id}, ${SPRINT2.id}`, JUST_AFTER_SPRINT_COMPLETE);
+    addSprintChange(issue, SPRINT1.id, `${SPRINT1.id}, ${SPRINT2.id}`, JUST_AFTER_SPRINT1_COMPLETE);
 
     const result = issueSprintReport(issue, SPRINT1);
     expect(result.outcome).toBe('NOT_COMPLETED');
@@ -323,9 +323,9 @@ describe('jiraSprintReport', () => {
     issue.fields.status = {
       name: 'Done',
     };
-    addSprintChange(issue, '', SPRINT1.id, BEFORE_SPRINT);
-    addSprintChange(issue, SPRINT1.id, `${SPRINT1.id}, ${SPRINT2.id}`, JUST_AFTER_SPRINT_COMPLETE);
-    addStatusChange(issue, 'To Do', 'Done', AFTER_SPRINT);
+    addSprintChange(issue, '', SPRINT1.id, BEFORE_SPRINT1);
+    addSprintChange(issue, SPRINT1.id, `${SPRINT1.id}, ${SPRINT2.id}`, JUST_AFTER_SPRINT1_COMPLETE);
+    addStatusChange(issue, 'To Do', 'Done', AFTER_SPRINT1);
 
     const result = issueSprintReport(issue, SPRINT1);
     expect(result.outcome).toBe('NOT_COMPLETED');
@@ -337,7 +337,7 @@ describe('jiraSprintReport', () => {
     issue.fields.status = {
       name: 'Done',
     };
-    addSprintChange(issue, '', SPRINT1.id, BEFORE_SPRINT);
+    addSprintChange(issue, '', SPRINT1.id, BEFORE_SPRINT1);
     addSprintChange(issue, SPRINT1.id, SPRINT2.id, DURING_SPRINT1);
 
     const result = issueSprintReport(issue, SPRINT1);
@@ -348,7 +348,7 @@ describe('jiraSprintReport', () => {
     const issue = makeMinimalIssue();
     issue.fields.customfield_sprint.push(SPRINT1);
     addStoryPointChange(issue, 3, 5, DURING_SPRINT1);
-    addSprintChange(issue, SPRINT2.id, SPRINT1.id, DURING_SPRINT2);
+    addSprintChange(issue, SPRINT2.id, SPRINT1.id, DURING_SPRINT1_2);
 
     const result = issueSprintReport(issue, SPRINT1);
     expect(result.initialEstimate).toBe(5);
@@ -392,8 +392,8 @@ describe('issueRemovedFromSprints', () => {
   test('Issue removed from 2 sprints', () => {
     const issue = makeMinimalIssue();
     addSprintChange(issue, '', SPRINT1.id, DURING_SPRINT1);
-    addSprintChange(issue, SPRINT1.id, SPRINT2.id, DURING_SPRINT2);
-    addSprintChange(issue, SPRINT2.id, '', AFTER_SPRINT);
+    addSprintChange(issue, SPRINT1.id, SPRINT2.id, DURING_SPRINT1_2);
+    addSprintChange(issue, SPRINT2.id, '', AFTER_SPRINT1);
 
     const sprintIds = issueRemovedFromActiveSprints(issue, SPRINTS_BY_ID);
 

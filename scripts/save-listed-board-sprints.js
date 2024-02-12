b47e81056396e0182c33af75e0107c0020d602e2
Sergey Trofimov <trofimov.s@gmail.com>
2024-02-12 21:58:57 +0100

    Save backlog issues for each board

--- scripts/save-listed-board-sprints.js
+++ scripts/save-listed-board-sprints.js
@@ -17,7 +17,6 @@ async function saveBoardData(board) {
 
   const boardPath = `data/board-${board.id}`;
 
-  // Create a new folder for the board
   if (!fs.existsSync(boardPath)) {
     fs.mkdirSync(boardPath);
   }
@@ -26,12 +25,20 @@ async function saveBoardData(board) {
   fs.writeFileSync(boardPath + '/sprints.json', JSON.stringify(sprints, null, 2));
   console.log(`Saved ${sprints.length} board sprints`);
 
-  // Get GreenHopper velocity report
   const velocity = await jiraGet(
     `rest/greenhopper/1.0/rapid/charts/velocity?rapidViewId=${board.id}`,
   );
   fs.writeFileSync(boardPath + '/velocity.json', JSON.stringify(velocity, null, 2));
   console.log('Saved GreenHopper velocity report');
+
+  const backlogIssues = await jiraGetItems(
+    `rest/agile/1.0/board/${board.id}/backlog` +
+      `?fields=status,${CUSTOM_FIELDS.sprint},${CUSTOM_FIELDS.storyPoints}` +
+      `&expand=changelog`,
+    'issues',
+  );
+  fs.writeFileSync(boardPath + '/backlog-issues.json', JSON.stringify(backlogIssues, null, 2));
+  console.log(`Saved ${backlogIssues.length} backlog issues`);
 
   for (const sprint of sprints) {
     await saveSprintData(board, sprint);
@@ -43,7 +50,6 @@ async function saveSprintData(board, sprint) {
 
   const sprintPathPrefix = `data/board-${board.id}/${sprint.id}-`;
 
-  // Get sprint issues
   const issues = await jiraGetItems(
     `rest/agile/1.0/board/${board.id}/sprint/${sprint.id}/issue` +
       `?fields=status,${CUSTOM_FIELDS.sprint},${CUSTOM_FIELDS.storyPoints}` +
@@ -53,7 +59,6 @@ async function saveSprintData(board, sprint) {
   fs.writeFileSync(sprintPathPrefix + 'sprint-issues.json', JSON.stringify(issues, null, 2));
   console.log(`Saved ${issues.length} issues`);
 
-  // Get GreenHopper sprint report
   const sprintReport = await jiraGet(
     `rest/greenhopper/1.0/rapid/charts/sprintreport?rapidViewId=${board.id}&sprintId=${sprint.id}`,
   );
